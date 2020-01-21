15e4ebe69f379269827e06394994fd7dc772cab0
Maryam Ghiasvand <m.ghiyasvand001@gmail.com>
2020-01-21 12:16:57 -0600

    Add a new window by the name of addNewBook

---
+++ LibraryProject/src/ui/AddNewBook.java
@@ -0,0 +1,26 @@
+package ui;
+
+import javafx.stage.Stage;
+
+public class AddNewBook extends Stage implements LibWindow{
+	public static final AddNewBook INSTANCE = new AddNewBook();
+	private  AddNewBook(){}
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isInitialized() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void isInitialized(boolean val) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- LibraryProject/src/ui/AllBooksWindow.java
+++ LibraryProject/src/ui/AllBooksWindow.java
@@ -45,6 +45,7 @@ public class AllBooksWindow extends Stage implements LibWindow {
 		ta = new TextArea();
 		grid.add(ta, 0,1);
 		Button backBtn = new Button("<= Back to Main");
+		Button newBookBtn = new Button("Add new book");
         backBtn.setOnAction(new EventHandler<ActionEvent>() {
         	@Override
         	public void handle(ActionEvent e) {
@@ -52,9 +53,20 @@ public class AllBooksWindow extends Stage implements LibWindow {
         		Start.primStage().show();
         	}
         });
+        newBookBtn.setOnAction(new EventHandler<ActionEvent>(){
+
+			@Override
+			public void handle(ActionEvent event) {
+				Start.hideAllWindows();
+				AddNewBook.INSTANCE.show();
+				
+			}
+        	
+        });
         HBox hBack = new HBox(10);
         hBack.setAlignment(Pos.BOTTOM_LEFT);
         hBack.getChildren().add(backBtn);
+        hBack.getChildren().add(newBookBtn);
         grid.add(hBack, 0, 2);
 		Scene scene = new Scene(grid);
 		scene.getStylesheets().add(getClass().getResource("library.css").toExternalForm());
