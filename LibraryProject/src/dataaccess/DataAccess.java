35ca51a8e725019a0f519b879e5a7acaf9eca2e8
Maryam Ghiasvand <m.ghiyasvand001@gmail.com>
2020-01-22 11:07:15 -0600

    Add saveNewBook method to DataAccessFacada

--- LibraryProject/src/dataaccess/DataAccess.java
+++ LibraryProject/src/dataaccess/DataAccess.java
@@ -11,4 +11,5 @@ public interface DataAccess {
 	public HashMap<String,User> readUserMap();
 	public HashMap<String, LibraryMember> readMemberMap();
 	public void saveNewMember(LibraryMember member); 
+	public void saveNewBook(Book book); 
 }
--- LibraryProject/src/ui/AddNewBook.java
+++ LibraryProject/src/ui/AddNewBook.java
@@ -1,19 +1,20 @@
 package ui;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import business.Address;
 import business.Author;
 import business.Book;
+import dataaccess.DataAccess;
+import dataaccess.DataAccessFacade;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -24,12 +25,18 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class AddNewBook extends Stage implements LibWindow{
+public class AddNewBook extends Stage implements LibWindow {
 	public static final AddNewBook INSTANCE = new AddNewBook();
+	private Stage dialogStage;
 	public Book book;
-	
-	
-	private  AddNewBook(){}
+
+	private AddNewBook() {
+	}
+
+	public void setDialogStage(Stage dialogStage) {
+		this.dialogStage = dialogStage;
+	}
+
 	@Override
 	public void init() {
 		setTitle("Add new book");
@@ -40,56 +47,60 @@ public class AddNewBook extends Stage implements LibWindow{
 		gp.setVgap(10);
 		gp.setPadding(new Insets(25, 25, 25, 25));
 		Text scenetitle = new Text("Add new book");
-		scenetitle.setFont(Font.font("Harlow Solid Italic", FontWeight.NORMAL, 20)); //Tahoma
+		scenetitle.setFont(Font.font("Harlow Solid Italic", FontWeight.NORMAL, 20)); // Tahoma
 		gp.add(scenetitle, 0, 0, 2, 1);
-		
-        Label lblTitle = new Label("Title :");
-        gp.add(lblTitle, 0, 1);
-        TextField txtTitle = new TextField();
-        gp.add(txtTitle, 1, 1);
-        
-        Label lblisbn = new Label("ISBN :");
-        gp.add(lblisbn, 0, 2);
-        TextField txtIsbn = new TextField();
-        gp.add(txtIsbn, 1, 2);
-        
-        Label lblmaxChkoutLength = new Label("Max checkout Lenght :");
-        gp.add(lblmaxChkoutLength, 0, 3);
-        TextField txtmaxChkoutLength = new TextField();
-        gp.add(txtmaxChkoutLength, 1, 3);
-        
-        gp.setGridLinesVisible(false) ;
-        Address ad = new Address("a","a","a","a");
-    	Author author = new Author("a", "b", "c", ad, "aa");
-    	List<Author> authors=new ArrayList<Author>();
-    	authors.add(author);
-        
+
+		Label lblTitle = new Label("Title :");
+		gp.add(lblTitle, 0, 1);
+		TextField txtTitle = new TextField();
+		gp.add(txtTitle, 1, 1);
+
+		Label lblisbn = new Label("ISBN :");
+		gp.add(lblisbn, 0, 2);
+		TextField txtIsbn = new TextField();
+		gp.add(txtIsbn, 1, 2);
+
+		Label lblmaxChkoutLength = new Label("Max checkout Lenght :");
+		gp.add(lblmaxChkoutLength, 0, 3);
+		TextField txtmaxChkoutLength = new TextField();
+		gp.add(txtmaxChkoutLength, 1, 3);
+
+		gp.setGridLinesVisible(false);
+		Address ad = new Address("a", "a", "a", "a");
+		Author author = new Author("a", "b", "c", ad, "aa");
+		List<Author> authors = new ArrayList<Author>();
+		authors.add(author);
+
 		Button backBtn = new Button("<= Back to Main");
 		Button saveBtn = new Button("Save");
 		Button addCopyBtn = new Button("Add copy");
 		saveBtn.setOnAction(new EventHandler<ActionEvent>() {
-        	@Override
+			@Override
 
-        	public void handle(ActionEvent e) {
-        		save(txtIsbn.getText(),txtTitle.getText(),Integer.parseInt(txtmaxChkoutLength.getText()),authors);
-        	}
-        });
-        backBtn.setOnAction(new EventHandler<ActionEvent>() {
-        	@Override
-        	public void handle(ActionEvent e) {
-        		Start.hideAllWindows();
-        		Start.primStage().show();
-        	}
-        });
-        HBox hBack = new HBox(10); 
-        hBack.setAlignment(Pos.BOTTOM_LEFT);
-        hBack.getChildren().add(backBtn);
-        hBack.getChildren().add(saveBtn);
-        hBack.getChildren().add(addCopyBtn);
-        gp.add(hBack, 1, 5);
+			public void handle(ActionEvent e) {
+				if (isInputValid(txtIsbn, txtTitle, txtmaxChkoutLength, authors)) {
+					save(txtIsbn.getText(), txtTitle.getText(), Integer.parseInt(txtmaxChkoutLength.getText()),
+							authors);
+					dialogStage.close();
+				}
+			}
+		});
+		backBtn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				Start.hideAllWindows();
+				Start.primStage().show();
+			}
+		});
+		HBox hBack = new HBox(10);
+		hBack.setAlignment(Pos.BOTTOM_LEFT);
+		hBack.getChildren().add(backBtn);
+		hBack.getChildren().add(saveBtn);
+		hBack.getChildren().add(addCopyBtn);
+		gp.add(hBack, 1, 5);
 		Scene scene = new Scene(gp);
 		scene.getStylesheets().add(getClass().getResource("library.css").toExternalForm());
-        setScene(scene);
+		setScene(scene);
 	}
 
 	@Override
@@ -101,53 +112,38 @@ public class AddNewBook extends Stage implements LibWindow{
 	@Override
 	public void isInitialized(boolean val) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	
 
-	private void save(String isbn, String title, int maxCheckoutLength, List<Author> authors){
-		//if (isInputValid()) {
-			book = new Book(isbn,title,maxCheckoutLength,authors);
-		
-			try {
-			File bookFile = new File("../src/dataaccess/storage/BOOKS.File");
-	        FileWriter bookWriter = new FileWriter(bookFile,true);
-	        bookWriter.write("Book1");
-//	        bookWriter.write(book.getClass().getFields().toString());
-
-	        bookWriter.close();
-			}
-			catch(IOException ex) {
-				ex.printStackTrace();
-			}
-		//}
-
-	} 
-	
-//	private boolean isInputValid() {
-//		String errorMessage = "";
-//		if (isbnFileld.getText() == null || isbnFileld.getText().length() == 0) {
-//			errorMessage += "Please enter Isbn!\n";
-//		}
-//		if(titleFileld.getText() == null || titleFileld.getText().length() ==0 ) {
-//			errorMessage += "Please enter title! \n";
-//		}
-//		if(maxCheckoutLengthFileld.getText() == null || maxCheckoutLengthFileld.getText().length() ==0) {
-//			errorMessage += "Please enter maxCheckoutLength \n";
-//		}
-//		if(errorMessage.length() ==0) {
-//			return false;
-//		}
-//		else {
-//			Alert alert = new Alert(AlertType.ERROR);
-//			alert.initOwner(dialogStage);
-//			alert.setTitle("Invalid Input");
-//			alert.setContentText(errorMessage);
-//			alert.showAndWait();
-//			return false;
-//		}
+	private void save(String isbn, String title, int maxCheckoutLength, List<Author> authors) {
+		// if (isInputValid()) {
+		book = new Book(isbn, title, maxCheckoutLength, authors);
+		DataAccess da = new DataAccessFacade();
+		da.saveNewBook(book);
+		// }
 	}
-	
 
-
+	private boolean isInputValid(TextField txtIsbn, TextField txtTitle, TextField txtmaxChkoutLength,
+			List<Author> authors) {
+		String errorMessage = "";
+		if (txtIsbn.getText() == null || txtIsbn.getText().length() == 0) {
+			errorMessage += "Please enter Isbn!\n";
+		}
+		if (txtTitle.getText() == null || txtTitle.getText().length() == 0) {
+			errorMessage += "Please enter title! \n";
+		}
+		if (txtmaxChkoutLength.getText() == null || txtmaxChkoutLength.getText().length() == 0) {
+			errorMessage += "Please enter maxCheckoutLength \n";
+		}
+		if (errorMessage.length() == 0) {
+			return false;
+		} else {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.initOwner(dialogStage);
+			alert.setTitle("Invalid Input");
+			alert.setContentText(errorMessage);
+			alert.showAndWait();
+			return false;
+		}
+	}
+}
